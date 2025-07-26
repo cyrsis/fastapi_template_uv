f464721dc0280b8521627d13dc66ca45e38d63f5
victor <em3888@gmail.com>
2025-07-26 15:17:42 +0800

    Try to deploy it with Docker Change from the import path

--- src/services/logs/__init__.py
+++ src/services/logs/__init__.py
@@ -2,9 +2,12 @@ import sys
 
 from loguru import logger
 
-from services.logs.handlers import UvicornHandler
+# from services.logs.handlers import UvicornHandler
+from src.services.logs.handlers import UvicornHandler
 
 __all__ = ["UvicornHandler", "configure_logger", "get_uvicorn_log_config"]
+
+
 
 
 def configure_logger() -> None:
