4582351a52b8210197c47c92059a829879e98250
victor <em3888@gmail.com>
2025-07-26 15:40:07 +0800

    make it always rediret the docs

--- src/app/app.py
+++ src/app/app.py
@@ -1,6 +1,39 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import RedirectResponse
 
 from app.routers import router as main_router
 
-app = FastAPI()
+app = FastAPI(title="Fast KS Send Email Server",
+              description="Send Email Service",
+              version="11.3.1",
+              contact={
+                  "name": "Victor Tong",
+                  "url": "http://x-force.example.com/contact/",
+                  "email": "victor@budgetapp.works",
+              },
+              license_info={
+                  "name": "Apache 2.0",
+                  "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+              }, )
+
+origins = [
+    "https://pwa.kszetta.com",
+    "http://localhost:8000",  # Allow origins (you may want to change this depending on your setup)
+    "http://localhost:8080",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # List of origins allowed (you can use ["*"] to allow all)
+    allow_credentials=True,  # Allows cookies to be sent with the requests
+    allow_methods=["*"],  # List of methods allowed (you can use ["*"] to allow all)
+    allow_headers=["*"],  # List of headers allowed (you can use ["*"] to allow all)
+)
+
+
+@app.get("/")
+async def docs_redirect():
+    return RedirectResponse(url='/docs')
+
+
 app.include_router(main_router)
